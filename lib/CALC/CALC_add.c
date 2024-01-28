30d0d8d37a2986d69c4d445cae4069dba17b277b
EltexHomeworkLevinDD <pykanus172@gmail.com>
2024-01-28 18:21:45 +0700

    work + screenshots

---
+++ lib/CALC/CALC_add.c
@@ -0,0 +1,6 @@
+#include "CALC.h"
+
+// Sum numbers (value1+value2)
+int CALC_add(int value1, int value2){
+    return value1 + value2;
+}
---
+++ lib/CALC/CALC_multiply.c
@@ -0,0 +1,6 @@
+#include "CALC.h"
+
+// Multiply numbers (value1*value2)
+int CALC_multiply(int value1, int value2){
+    return value1 * value2;
+}
